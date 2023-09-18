ab54fbf0d557bf90fd875b16ed64e6c71218da01
mihad-khadem <mihadkhadem@gmail.com>
2023-09-19 02:16:13 +0600

    Selected Option Toggled

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
 import './cart.css'
 
-const Cart = () => {
+const Cart = ({selectedActors}) => {
+    
     return (
         <div className=''>
-            <h1>Cart</h1>
+            
+            <h1>Selected</h1>
+            <h3>Total Selected: {selectedActors.length}</h3>
+
+            <ol>
+                {
+                    selectedActors.map((actor) => (
+                        <li>name: {actor.name} <br />
+                        price: {actor.salary}
+                        </li>
+                    ))
+                }
+            </ol>
+
         </div>
     );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,53 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from 'react';
 import Cart from '../cart/Cart';
 import './home.css'
 
 const Home = () => {
+    const [actors, setActors] = useState([]);
+    const [selectedActors, setSelctedActors] = useState([]);
+
 
     useEffect(() =>{
         fetch("./data.json")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setActors(data))
     }, [])
+
+
+    const handleSelectActor = (actor) =>{
+        
+        setSelctedActors([...selectedActors, actor]);
+    };
+    
+
+    
+    
 
     return (
         <div className='container'>
+            <div className="home-container">
             <div className="card-container">
-                <div className="card">
-                    <div className="card-img">
-                        <img className='photo' src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" />
-                    </div>
-                </div>
+                {
+                    actors.map((actor) => (
+                        <div key={actor.id} actor={actor} className="card">
+                            <div className="card-img">
+                                <img className='photo' src={actor.image} alt="" />
+                            </div>
+                            <h2>Name: {actor.name}</h2>
+                            <h4>Profession: {actor.role}</h4>
+                            <p>Salary: ${actor.salary}</p>
+                            <button onClick={() =>handleSelectActor(actor)}>Select</button>
+                            
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='cart'>
+                <Cart selectedActors={selectedActors}></Cart>
+
+                
+            </div>
             </div>
         </div>
     );
